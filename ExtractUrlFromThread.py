a145bd8021fe293a7e77b272f94b20fb1aae7619
Sevak Mardirosian <smardirosians@gmail.com>
2017-07-16 21:58:22 +0200

    using BeautifulSoup to finish the job.

--- ExtractUrlFromThread.py
+++ ExtractUrlFromThread.py
@@ -1,19 +1,48 @@
-import os, sys
+import requests
+from BeautifulSoup import BeautifulSoup
+import urllib2
+import re
 
-#Object: Scan a thread of a given site (HN) and save all
+#https://stackoverflow.com/questions/15517483/how-to-extract-urls-from-an-html-page-in-python
+#https://stackoverflow.com/questions/3075550/how-can-i-get-href-links-from-html-using-python
+
+#Objective: Scan a thread of a given site (HN) and save all
 #save all the url into a text file.
 
 class ExtractUrlFromThread():
 
     def __construct__(self):
-        self.extract('text')
+        pass
 
 
-    def extract(self, url):
-        print(url)
+    def extract(self, page):
+        """
+        :param page: html of web page
+        :return: urls in that page
+        """
+        
+        start_link = page.find("href")
+        if start_link == -1:
+            return None, 0
+        start_quote = page.find('"', start_link)
+        end_quote = page.find('"', start_quote + 1)
+        url = page[start_quote + 1: end_quote]
 
+        return url, end_quote
 
-
+    
+    
 if __name__ == "__main__":
+    
     main = ExtractUrlFromThread()
-    main.extract('tests')
+    url = "https://news.ycombinator.com/item?id=14437921"
+    response = requests.get(url)
+    page = str(BeautifulSoup(response.content))
+    
+    while True:
+        url, n = main.extract(page)
+        page = page[n:]
+        if url:
+            print(url)
+        else:
+            break
