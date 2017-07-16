986d796e19c2dda563f8ef866854de522fa0f9f7
Sevak Mardirosian <smardirosians@gmail.com>
2017-07-16 22:15:05 +0200

    adding user-agent to the download function.

--- Scraper.py
+++ Scraper.py
@@ -1,14 +1,22 @@
-import sys, os, urllib.request
+import urllib.request
 from urllib.error import URLError, HTTPError, ContentTooShortError
+
 
 class Scraper():
     
     def __construct__(self):
         pass
 
-    def download(self, url, num_retries=2):
-        """When the url is passed, this function will download the page and return html""" 
+    def download(self, url, user_agent='zeroH', num_retries=2):
+        """
+        When the url is passed, this function will
+        download the page and return html
+        """ 
         print('Downloading: ', url)
+        
+        request = urllib.request.Request(url)
+        request.add_header('User-agent', user_agent)
+        
         try:
             html = urllib.request.urlopen(url).read()
         except (URLError, HTTPError, ContentTooShortError) as e:
@@ -20,9 +28,17 @@ class Scraper():
                     return self.download(url, num_retries - 1)
                     
         print(html)
+
+    def save_into_file(self, data):
+        """
+        :param data
+        :return file
+        Saving the urls into file
+        """
+        pass
     
     
 if __name__ == '__main__':
     main = Scraper()
-    main.download('http://httpstat.us/500')
+    main.download('https://meetup.com')
     
