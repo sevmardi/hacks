4ca194970476bbec87abee242109637afcf19739
Sevak Mardirosian <smardirosians@gmail.com>
2017-09-30 08:23:50 +0200

    testing ssh

--- tcp_server.py
+++ tcp_server.py
@@ -13,6 +13,7 @@ server.listen(5)
 
 print("[*] Listening on %s:%d" % (bind_ip, bind_port))
 
+
 def handle_client(client_socket):
     #print out what the client sends 
     request = client_socket.recv(1024)
@@ -21,7 +22,7 @@ def handle_client(client_socket):
     client_socket.send("ACK")
     client_socket.close()
 
-    
+
 while True:
     client, addr = server.accept()
     print("[*] Accepted connection from: %s:%d" (addr[0], addr[1]))        
